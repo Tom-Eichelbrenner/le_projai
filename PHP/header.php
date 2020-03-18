78b15c5e4a63ea7fadcbf9296cbd0fbe81f6d199
Tom-Eichelbrenner <tom.eichelbrenner01@gmail.com>
2020-03-18 16:16:26 +0100

    creation of PHP dir + header/footer/form and including them to all 3 pages

---
+++ PHP/header.php
@@ -0,0 +1,22 @@
+<?php
+?>
+
+<!--HEADER-->
+<header>
+    <nav>
+        <div class="nav_logo">
+            <a href="../accueil/index.php">
+                <img src="../images/photos/logo (2).png" alt="logo">
+            </a>
+        </div>
+        <div class="nav_links">
+            <a href="">Magic Leap 1</a>
+            <a href="../Experiences_Page/experiences.php">Experiences</a>
+            <a href="">Entreprise</a>
+            <a href="">Developer</a>
+            <a href="../Shop_Page/index_shop.php" class="myButton">Shop</a>
+        </div>
+    </nav>
+</header>
+<div class="nav_help"></div>
+<!--HEADER-->
---
+++ Shop_Page/index_shop.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <script src="https://kit.fontawesome.com/c5a11bbba4.js" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../reset.css">
+    <link rel="stylesheet" href="style_shop.css">
+</head>
+
+<body>
+    <!--HEADER-->
+    <?php
+    include '../PHP/header.php'
+    ?>
+    <!--HEADER-->
+    <!--MA PAGE-->
+    <div class="title_block">
+        <h1>Shop Magic Leap 1</h1>
+        <p>Magic Leap 1 is available for individuals, developers, and groundbreaking businesses.</p>
+    </div>
+    <div class="shop_boxes">
+        <div class="shop_box dsk4 phn12">
+            <img src="Images/box1.png" alt="">
+            <h3>Magic Leap 1</h3>
+            <p>Starting a $96/ month 0% APR with affirm</p>
+            <a href="">Learn More</a>
+            <div class="box_price">
+                <p>$2,295</p>
+                <button>Add to Cart</button>
+            </div>
+            <div class="box_features">
+                <h4>What's included ?</h4>
+                <ul>
+                    <li>Magic Leap 1 (Lightwear, Lightpack and Control)</li>
+                    <li>Lightpack Charger</li>
+                    <li>Control Charger</li>
+                    <li>2 USB-C Cables</li>
+                    <li>AC Power Cable</li>
+                    <li>Wrist Lanyard</li>
+                    <li>Cleaning Cloth</li>
+                    <li>Shoulder Strap</li>
+                    <li>Fit Kit (includes additional pads)</li>
+                    <li>1 Year Limited Warranty</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="shop_box dsk4 phn12">
+            <img src="Images/box2.png" alt="">
+            <h3>Developer Suite</h3>
+            <p>Starting a $104/ month 0% APR with affirm</p>
+            <a href="">Learn More</a>
+            <div class="box_price">
+                <p>$2,495</p>
+                <button>Add to Cart</button>
+            </div>
+            <div class="box_features">
+                <h4>What's included ?</h4>
+                <ul>
+                    <li>Magic Leap 1 (Lightwear, Lightpack and Control)</li>
+                    <li>Lightpack Charger</li>
+                    <li>Control Charger</li>
+                    <li>2 USB-C Cables</li>
+                    <li>AC Power Cable</li>
+                    <li>Wrist Lanyard</li>
+                    <li>Cleaning Cloth</li>
+                    <li>Shoulder Strap</li>
+                    <li>Fit Kit (includes additional pads)</li>
+                    <li>1 Year Limited Warranty</li>
+                    <li>Carry Case</li>
+                    <li>Developer Hub</li>
+                    <li>1 Year RapidReplace</li>
+                    <li>1 Year Limited Warranty</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="shop_box dsk4 phn12">
+            <img src="Images/box3.png" alt="">
+            <h3>Entreprise suite</h3>
+            <p>Only available for Enterprise customers.</p>
+            <a href="">Learn More</a>
+            <div class="box_price">
+                <p>$2,995</p>
+                <button>Learn more</button>
+            </div>
+            <div class="box_features">
+                <h4>What's included ?</h4>
+                <ul>
+                    <li>Magic Leap 1 (Lightwear, Lightpack and Control)</li>
+                    <li>Lightpack Charger</li>
+                    <li>Control Charger</li>
+                    <li>2 USB-C Cables</li>
+                    <li>AC Power Cable</li>
+                    <li>Wrist Lanyard</li>
+                    <li>Cleaning Cloth</li>
+                    <li>Shoulder Strap</li>
+                    <li>Fit Kit (includes additional pads)</li>
+                    <li>2 Years Device Manager access</li>
+                    <li>2 Years Limited Warranty</li>
+                    <li>2 Years RapidReplace</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="info_boxes">
+        <div class="info_item dsk3 tbl6 phn12">
+            <i class="fas fa-truck"></i>
+            <h3>Free, fast delivery</h3>
+            <p>Enjoy free shipping all over the US or purchase from your local AT&T store.</p>
+        </div>
+        <div class="info_item dsk3 tbl6 phn12">
+            <i class="far fa-calendar-alt"></i>
+            <h3>Buy today, pay later</h3>
+            <p>Spread the cost of Magic Leap 1 over multiple payments, starting at $96/month at 0% APR with affirm.</p>
+        </div>
+        <div class="info_item dsk3 tbl6 phn12">
+            <i class="far fa-check-square"></i>
+            <h3>Extended services</h3>
+            <p>Up to 2 years access to RapidReplace, so if there's a problem you'll get a super fast replacement device.
+            </p>
+        </div>
+        <div class="info_item dsk3 tbl6 phn12">
+            <i class="fas fa-phone-square-alt"></i>
+            <h3>Need help ?</h3>
+            <p>Give us a call on 833-45-MAGIC (833-456-2442) or chat online.</p>
+        </div>
+    </div>
+    <div class="FAQ">
+        <div class="FAQ_title">
+            <h2>Frequently Asked Questions</h2>
+        </div>
+        <div class="FAQ_items">
+            <div class="FAQ_item dsk6 tbl12 phn12">
+                <h4>Can I wear my glasses with Magic Leap1?</h4>
+                <p>You can't wear glasses with Magic Leap 1, but thanks to the Prescription Insert, you don't have to.
+                    It slots magnetically into Magic Leap 1 to give you the best possible viewing experience. Learn
+                    more.</p>
+            </div>
+            <div class="FAQ_item dsk6 tbl12 phn12">
+                <h4>How do I know which size Lightwear to buy?</h4>
+                <p>Finding the right Lightwear size is based on IPD (interpupillary distance), which sounds tricky but
+                    it's actually very simple to calculate. If you wear glasses, your optician can tell you. Or download
+                    the Magic Leap Mobile App to discover yours. Still have questions?</p>
+            </div>
+            <div class="FAQ_item dsk6 tbl12 phn12">
+                <h4>Will I get a warranty with my Magic Leap1?</h4>
+                <p>Absolutely. Every Magic Leap 1 comes with a 1 year hardware repair coverage provided by our 1-Year
+                    Limited Warranty Policy.</p>
+            </div>
+            <div class="FAQ_item dsk6 tbl12 phn12">
+                <h4>Can I order more then 9?</h4>
+                <p>We like your enthusiasm. If you'd like to order more than 9 Magic Leap devices for your business,
+                    please fill out the Enterprise Qualification Form and a member of our team will be in touch shortly.
+                    You can also reach out to our Customer Care team at 833-456-2442.</p>
+            </div>
+        </div>
+    </div>
+    <!--MA PAGE-->
+    <?php
+    include '../PHP/form.php';
+    include '../PHP/footer.php'
+    ?>
+
+    <script src="../base/base.js"></script>
+</body>
+
+</html>
